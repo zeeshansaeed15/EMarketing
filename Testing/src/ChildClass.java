5df3055f1c7ce53db82c828ec319a50a87e815f6
Zeeshan <zeeshansaeed15@yahoo.com>
2015-10-26 14:58:17 +0000

    very new commit

--- Testing/src/ChildClass.java
+++ Testing/src/ChildClass.java
@@ -6,7 +6,7 @@ import org.test.ParentClass;
 public class ChildClass extends ParentClass {
 	public static void main(String args){
 		ChildClass child = new ChildClass();
-		child.test1();dfdffdsdfsdf
+		child.test1();dfdffdsdfsdfdfssfdsdf
 		ParentClass parent = new ChildClass();
 	}
 }
